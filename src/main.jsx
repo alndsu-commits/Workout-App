901154c76eac83e54a9fad7f6aa7e468ed8bab1b
alndsu-commits <al.ndsu@gmail.com>
2026-03-10 10:51:13 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WorkoutApp from './WorkoutApp'
+ReactDOM.createRoot(document.getElementById('root')).render(<WorkoutApp />)
