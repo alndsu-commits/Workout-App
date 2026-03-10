3319c71633baa605568705eb42f4781ce1655ea2
alndsu-commits <al.ndsu@gmail.com>
2026-03-10 11:33:47 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 import WorkoutApp from './WorkoutApp'
 ReactDOM.createRoot(document.getElementById('root')).render(<WorkoutApp />)
